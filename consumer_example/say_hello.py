2c93e06eb587c2c14046ba045c52076204425296
Lucas Silva <lucas.silva@fontespromotora.com.br>
2022-03-24 15:46:47 -0300

    Refatoração do consumer

--- consumer_example/say_hello.py
+++ consumer_example/say_hello.py
@@ -1,16 +1,10 @@
-from dataclasses import dataclass
-import json
-from logging import StreamHandler, getLogger
-import logging
-from time import sleep
+from logging import getLogger
 from typing import List
 
 from pydantic import BaseModel
-from logging_utils import CustomFormatter
-
-from tasks_manager_python.consumer import TaskExecutionData, TaskConsumer, TaskPayloadParser
-from tasks_manager_python.consumer.providers.rabbitmq import RabbitMQTaskMetadata, RabbitMQTaskProvider
-import pika
+from tasks_manager_python.consumer import TaskPayloadParser
+from tasks_manager_python.rabbitmq.app import (BasicRabbitMQApplication,
+                                               RMQConfig)
 
 logger = getLogger(__name__)
 
@@ -29,82 +23,21 @@ class SayHelloParser(TaskPayloadParser[SayHelloPayload]):
         return SayHelloPayload(**payload)
 
 
-@dataclass
-class RMQConfig:
-    host: str
-    port: int
-    user: str
-    password: str
-    queue: str
-
-
-class SayHelloApplication:
-    def __init__(self, rmq_config: RMQConfig):
-
-        self.parser = SayHelloParser()
-
-        self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(
-                host=rmq_config.host,
-                port=rmq_config.port,
-                credentials=pika.PlainCredentials(
-                    rmq_config.user, rmq_config.password)
-            )
-        )
-
-        self.channel = self.connection.channel()
-        self.channel.queue_declare(queue=rmq_config.queue)
-
-        self.consumer = TaskConsumer(
-            self.parser,
-            self.on_message,
-            task_provider=RabbitMQTaskProvider(
-                self.channel,
-                rmq_config.queue
-            )
-        )
-
-        self.logger = getLogger(f'{__name__}.{self.__class__.__name__}')
-
-        stream_handler = StreamHandler()
-        stream_formatter = CustomFormatter(
-            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        stream_handler.setFormatter(stream_formatter)
-        stream_handler.setLevel(logging.DEBUG)
-
-        self.logger.addHandler(stream_handler)
-
-        self.logger.addHandler(self.consumer.get_logger_handler())
-
-        self.logger.info('SayHelloApplication initialized')
-        self.logger.setLevel(logging.DEBUG)
-
-    def on_message(self, context: TaskExecutionData[SayHelloPayload, RabbitMQTaskMetadata]):
-
-        self.logger.debug(f'Executing callback with context: {context}')
-        self.logger.info(
-            f"Hello {context.payload.name}!, I see that youm have {len(context.payload.hobbies)} hobbies")
-        self.logger.info(
-            f"They are: {', '.join([hobbie.name for hobbie in context.payload.hobbies])}")
-        self.logger.info("waiting 10 seconds")
-        sleep(10)
-        self.logger.info("done")
-
-    def run(self):
-        self.logger.info('SayHelloApplication started')
-        self.logger.debug('Consuming messages')
-        self.consumer.consume_messages()
-
-
 if __name__ == '__main__':
     rmq_config = RMQConfig(
         host='localhost',
         port=5672,
         user='guest',
         password='guest',
-        queue='hello_queue'
+        messages_queue='hello_queue',
+        logs_queue='hello_logs_queue'
     )
-    application = SayHelloApplication(
-        rmq_config
+
+    application = BasicRabbitMQApplication(
+        rmq_config=rmq_config,
+        parser=SayHelloParser(),
+        callback=lambda task_execution_data: logger.info(
+            f'Task execution data: {task_execution_data}')
     )
+
     application.run()
